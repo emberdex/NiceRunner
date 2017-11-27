0448594961f3bc7fb0d1f911fadbfad9def3cb6e
Toby Jones <psytj1@exmail.nottingham.ac.uk>
2017-11-27 19:18:35 +0000

    Added stdout and stderr redirection to file.

--- nicerunner.py
+++ nicerunner.py
@@ -23,6 +23,7 @@ class RunnerData:
 	iterations = 10
 	executable = ""
 	time_limit = 0
+	save_file = False
 
 # Function to parse command line arguments.
 def parse_arguments():
@@ -33,6 +34,7 @@ def parse_arguments():
 	argument_parser.add_argument("executable", help="The executable to run.")
 	argument_parser.add_argument("-n", "--number-iterations", help="The number of iterations to run.")
 	argument_parser.add_argument("-t", "--time-limit", help="The time to wait before considering the process as locked, in seconds.")
+	argument_parser.add_argument("-s", "--save-output", help="Save stdout and stderr to a file.", action="store_true")
 
 	# Read the passed-in arguments and parse them.
 	args = argument_parser.parse_args()
@@ -54,6 +56,9 @@ def parse_arguments():
 			print("Timeout value must be an integer.")
 			sys.exit(1)
 
+	if args.save_output:
+		RunnerData.save_file = True
+
 def execute():
 	# Check if the path exists.
 	if not os.path.exists(RunnerData.executable):
@@ -67,15 +72,36 @@ def execute():
 
 	print("Going to execute file \"{}\" {} times.".format(RunnerData.executable, RunnerData.iterations))
 
+	stderr_file = ""
+	stdout_file = ""
+
+	if RunnerData.save_file:
+		stderr_filename = "{}.stdout.log".format(RunnerData.executable)
+		stdout_filename = "{}.stderr.log".format(RunnerData.executable)
+
+		stderr_file = open(stderr_filename, "w")
+		stdout_file = open(stdout_filename, "w")
+
+		print("Logging to {} and {}.".format(stdout_filename, stderr_filename))
+		stdout_file.write("=* Running process {} {} times. *=".format(RunnerData.executable, RunnerData.iterations))
+
 	for x in range(0, RunnerData.iterations):
 		start = time.time()
 		timed_out = False
-		process = subprocess.Popen(RunnerData.executable, stdout=subprocess.PIPE, preexec_fn=os.setsid)
+		process = ""
+
+		if RunnerData.save_file:
+			process = subprocess.Popen(RunnerData.executable, stdout=stdout_file, stderr=stderr_file, preexec_fn=os.setsid)
+		else:
+			process = subprocess.Popen(RunnerData.executable, stdout=subprocess.PIPE, stderr=subprocess.PIPE, preexec_fn=os.setsid)
 
 		while process.poll() == None:
 			if RunnerData.time_limit > 0 and (time.time() - start) > RunnerData.time_limit:
 				os.killpg(os.getpgid(process.pid), signal.SIGKILL)
 				timed_out = True
+
+				stderr_file.write("=* Iteration {} timed out and was killed after {} seconds. *=".format(x, RunnerData.time_limit))
+
 				continue
 
 		end = time.time()
@@ -87,6 +113,12 @@ def execute():
 			print("{}Iteration {} took {} seconds.{}".format(ANSI.green, x, timeval, ANSI.reset), end="")
 			print("{} (return value: {}){}".format(ANSI.yelw, process.returncode, ANSI.reset), end="")
 			print("")
+
+		stderr_file.flush()
+		stdout_file.flush()
+
+	stdout_file.close()
+	stderr_file.close()
 
 parse_arguments()
 execute()
